1bb6aa70952ac2168ccd68f8f81b01dd19f5fc5c
flimby2010 <87784870+flimby2010@users.noreply.github.com>
2022-07-18 15:38:29 -0300

    .

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./pages.css";
+import "./pages.module.css";
 
 const Contato = () => {
   return (
     <div className="style">
       <h1>Contato</h1>
+      <p>
+        Entre em contato através do nosso e-mail lulupulinhas@gmail.com ou
+        acesse nossas redes socias!
+      </p>
     </div>
   );
 };
--- src/pages/quem.js
+++ src/pages/quem.js
@@ -1,10 +1,21 @@
 import React from "react";
-import "./pages.css";
 
 const QuemSomos = () => {
   return (
-    <div>
-      <h1>quem somos</h1>
+    <div className="quemsomos">
+      <div className="title">
+        <h1> Quem somos </h1>
+      </div>
+      <div className="main">
+        <p>
+          Somos um grupo de 60 mulheres que se reuniu, em 2015, para conhecer
+          mais sobre a cultura cervejeira e compartilhar momentos,
+          confraternizando e fortalecendo amizades. O grupo, criado pela
+          sommelière Lindslei Antunes, já realizou mais de 46 eventos, entre
+          encontros e viagens, sempre relacionados ao universo da cerveja
+          artesanal.
+        </p>
+      </div>
     </div>
   );
 };
